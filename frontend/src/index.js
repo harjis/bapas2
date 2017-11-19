6b01c69d94234dec9d0ea72c2bfed936be0bb444
Joonas Harjukallio <harjukallio@gmail.com>
2017-11-19 17:30:32 +0200

    Started with Bapas app

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,29 +1,6 @@
-import _ from 'lodash';
-import printMe from './print.js';
-import './style.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-function component() {
-  var element = document.createElement('div');
-  var btn = document.createElement('button');
+import App from './app';
 
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
-  btn.innerHTML = 'Click me and check the console!';
-  btn.onclick = printMe;
-
-  element.appendChild(btn);
-
-  return element;
-}
-
-let element = component(); // Store the element to re-render on print.js changes
-document.body.appendChild(element);
-
-if (module.hot) {
-  module.hot.accept('./print.js', function() {
-    console.log('Accepting the updated printMe module!');
-    document.body.removeChild(element);
-    element = component(); // Re-render the "component" to update the click handler
-    document.body.appendChild(element);
-  })
-}
+ReactDOM.render(<App/>, document.getElementById('app'));
--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -3,9 +3,17 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const webpack = require('webpack');
 
+const paths = {
+  DIST: path.resolve(__dirname, 'dist'),
+  SRC: path.resolve(__dirname, 'src')
+};
+
 module.exports = {
   entry: {
-    app: './src/index.js'
+    app: path.join(paths.SRC, 'index.js')
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
   },
   devtool: 'inline-source-map',
   devServer: {
@@ -14,18 +22,23 @@ module.exports = {
   },
   plugins: [
     new CleanWebpackPlugin(['dist']),
-    new HtmlWebpackPlugin({
-      title: 'Output Management'
-    }),
+    new HtmlWebpackPlugin({ template: path.join(paths.SRC, 'index.html') }),
     new webpack.NamedModulesPlugin(),
     new webpack.HotModuleReplacementPlugin()
   ],
   output: {
     filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist')
+    path: paths.DIST
   },
   module: {
     rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: [
+          'babel-loader',
+        ],
+      },
       {
         test: /\.css$/,
         use: ['style-loader', 'css-loader']
