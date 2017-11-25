bf5669a56123da6f1397109945e5ab5e91fec19d
Joonas Harjukallio <harjukallio@gmail.com>
2017-11-25 11:48:15 +0200

    Add routing

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -1,8 +1,20 @@
 import React from 'react';
+import { HashRouter as Router, Route } from 'react-router-dom';
+
 import Header from './components/header/header';
 
 export default class App extends React.Component {
   render() {
-    return <Header />;
+    return (
+      <Router hashType="noslash">
+        <div>
+          <Header />
+
+          <Route exact path="" />
+          <Route path="calendar" />
+          <Route path="accounts" />
+        </div>
+      </Router>
+    );
   }
 }
--- frontend/src/components/header/header.js
+++ frontend/src/components/header/header.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import './header.css';
 
 export default class Header extends React.Component {
+  item({ label, url }) {
+    return (
+      <Link key={url} to={url}>
+        <div styleName="item">{label}</div>
+      </Link>
+    );
+  }
+
   render() {
-    return <div styleName="test">Header</div>
+    const items = [
+      { label: 'Icon home', url: '' },
+      { label: 'Calendar', url: 'calendar' },
+      { label: 'Accounts', url: 'accounts' }
+    ];
+    return <div styleName="container">{items.map(this.item)}</div>;
   }
 }
