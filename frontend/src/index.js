1191b0b43984443684feee0806072f541041953c
Joonas Harjukallio <harjukallio@gmail.com>
2018-02-11 21:44:23 +0200

    Add apollo client to frontend

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { ApolloProvider } from 'react-apollo';
+import { ApolloClient } from 'apollo-client';
+import { HttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
 
-import App from './app';
+const httpLink = new HttpLink({ uri: 'http://localhost:4000' });
 
-ReactDOM.render(<App/>, document.getElementById('app'));
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache()
+});
+
+import Header from './components/header/header';
+import Accounts from './components/accounts/accounts';
+import Workspace from './components/workspace/workspace';
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <Router>
+      <div>
+        <Header />
+        <Switch>
+          <Route exact path="/" component={Workspace} />
+          <Route path="/accounts" component={Accounts} />
+        </Switch>
+      </div>
+    </Router>
+  </ApolloProvider>,
+  document.getElementById('app')
+);
