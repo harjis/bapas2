b4e473ca706f19a2c095d2e664157b08a262aecf
Joonas Harjukallio <harjukallio@gmail.com>
2018-03-06 19:14:37 +0200

    Add a prtötötöt... HTML table! :D

--- frontend/src/components/accounts/accounts.js
+++ frontend/src/components/accounts/accounts.js
@@ -1,18 +1,41 @@
 import * as React from 'react';
 import { graphql } from 'react-apollo';
 import gql from 'graphql-tag';
+import PropTypes from 'prop-types';
 
 import Loading from 'src/components/generic/loading/loading';
 
+import styles from './accounts.module.css';
+
+const TableRow = account => (
+  <tr key={account.iban}>
+    <td>{account.name}</td>
+    <td>{account.iban}</td>
+  </tr>
+);
 const Accounts = props => {
   if (props.data.loading) return <Loading />;
 
   return (
-    <div>
-      <div>Accounts</div>
-      <div>{props.data.accounts.map(account => account.name)}</div>
+    <div className={styles.container}>
+      <div className={styles.titleContainer}>Accounts</div>
+      <div>
+        <table>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>IBAN</th>
+            </tr>
+          </thead>
+          <tbody>{props.data.accounts.map(TableRow)}</tbody>
+        </table>
+      </div>
     </div>
   );
+};
+
+Accounts.propTypes = {
+  data: PropTypes.object.isRequired
 };
 
 export default graphql(gql`
