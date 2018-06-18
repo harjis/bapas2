7ee7de3c16201392cd0e148516406af592952ead
Joonas Harjukallio <harjukallio@gmail.com>
2018-06-19 00:40:52 +0300

    Use .evn

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,16 +1,17 @@
-const { GraphQLServer } = require('graphql-yoga');
-const { Prisma } = require('prisma-binding');
-const resolvers = require('./resolvers');
+const { GraphQLServer } = require('graphql-yoga')
+const { Prisma } = require('prisma-binding')
+const resolvers = require('./resolvers')
+
+const db = new Prisma({
+  typeDefs: 'src/generated/prisma.graphql',
+  endpoint: 'http://localhost:4466',
+  debug: true
+})
 
 const server = new GraphQLServer({
-  typeDefs: 'src/schema.graphql',
+  typeDefs: './src/schema.graphql',
   resolvers,
-  context: req => ({
-    ...req,
-    prisma: new Prisma({
-      typeDefs: 'src/generated/prisma.graphql',
-      endpoint: 'http://localhost:4466',
-    }),
-  }),
+  context: req => ({ ...req, db }),
 })
-server.start(() => console.log(`GraphQL server is running on http://localhost:4000`))
+
+server.start(() => console.log('Server is running on http://localhost:4000'))
