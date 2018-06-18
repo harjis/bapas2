c0e80b4d947a3c939feb103230539136b17acaec
Joonas Harjukallio <harjukallio@gmail.com>
2018-06-19 00:40:52 +0300

    Use .evn

--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -2,16 +2,16 @@ const { getUserId } = require('../utils');
 
 const Query = {
   accounts(parent, args, ctx, info) {
-    getUserId(ctx)
-    return ctx.db.query.accounts({}, info)
+    getUserId(ctx);
+    return ctx.db.query.accounts({}, info);
   },
   currentUser(parent, args, ctx, info) {
-    const id = getUserId(ctx)
-    return ctx.db.query.user({ where: { id } }, info)
+    const id = getUserId(ctx);
+    return ctx.db.query.user({ where: { id } }, info);
   },
   users(parent, args, ctx, info) {
-    getUserId(ctx)
-    return ctx.db.query.users({}, info)
+    getUserId(ctx);
+    return ctx.db.query.users({}, info);
   }
 };
 
