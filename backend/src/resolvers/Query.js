9858fb0a1b1df25da9872c253e09cc657209192c
Joonas Harjukallio <harjukallio@gmail.com>
2018-06-19 23:03:02 +0300

    Add backend support for Upload

--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -1,18 +1,24 @@
 const { getUserId } = require('../utils');
+const { db } = require('../file_upload');
 
 const Query = {
-  accounts(parent, args, ctx, info) {
-    getUserId(ctx);
-    return ctx.db.query.accounts({}, info);
-  },
-  currentUser(parent, args, ctx, info) {
-    const id = getUserId(ctx);
-    return ctx.db.query.user({ where: { id } }, info);
-  },
-  users(parent, args, ctx, info) {
-    getUserId(ctx);
-    return ctx.db.query.users({}, info);
+    accounts(parent, args, ctx, info) {
+      getUserId(ctx);
+      return ctx.db.query.accounts({}, info);
+    },
+    currentUser(parent, args, ctx, info) {
+      const id = getUserId(ctx);
+      return ctx.db.query.user({ where: { id } }, info);
+    },
+    users(parent, args, ctx, info) {
+      getUserId(ctx);
+      return ctx.db.query.users({}, info);
+    },
+    uploads() {
+      getUserId(ctx);
+      return db.get('uploads').value();
+    }
   }
-};
+;
 
 module.exports = { Query };
