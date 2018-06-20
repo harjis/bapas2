cd44c1f08e08ef1b7099e9cf77eb1ca770809950
Joonas Harjukallio <harjukallio@gmail.com>
2018-06-20 21:03:57 +0300

    Add backend support for file removal

--- backend/src/file_upload.js
+++ backend/src/file_upload.js
@@ -1,10 +1,10 @@
-const { createWriteStream } = require('fs');
+const { createWriteStream, unlinkSync } = require('fs');
 const mkdirp = require('mkdirp');
 const shortid = require('shortid');
 const lowdb = require('lowdb');
 const FileSync = require('lowdb/adapters/FileSync');
 
-const uploadDir = './uploads/';
+const uploadDir = './uploads';
 const db = new lowdb(new FileSync('db.json'));
 
 // Seed an empty DB
@@ -13,9 +13,11 @@ db.defaults({ uploads: [] }).write();
 // Ensure upload directory exists
 mkdirp.sync(uploadDir);
 
-const storeUpload = async ({ stream, filename }) => {
+const getFilePath = (id) => `${uploadDir}/${id}.txt`;
+
+const saveFileToFilesystem = async ({ stream }) => {
   const id = shortid.generate();
-  const path = `${uploadDir}/${id}-${filename}`;
+  const path = getFilePath(id);
 
   return new Promise((resolve, reject) =>
     stream
@@ -25,20 +27,47 @@ const storeUpload = async ({ stream, filename }) => {
   );
 };
 
-const recordFile = file =>
+const saveFileToDB = file =>
   db
     .get('uploads')
     .push(file)
     .last()
     .write();
 
-const processUpload = async upload => {
+const saveFile = async upload => {
   const { stream, filename, mimetype, encoding } = await upload;
-  const { id, path } = await storeUpload({ stream, filename });
-  return recordFile({ id, filename, mimetype, encoding, path });
+  const { id, path } = await saveFileToFilesystem({ stream });
+  return saveFileToDB({ id, filename, mimetype, encoding, path });
+};
+
+const deleteFile = async id => {
+  await deleteFileFromFilesystem({ id });
+  deleteFileFromDB({ id });
+  return id;
+};
+
+const deleteFileFromFilesystem = async ({ id }) => {
+  try {
+    unlinkSync(getFilePath(id));
+  } catch (e) {
+    return false;
+  }
+
+  return true;
+};
+
+const deleteFileFromDB = id => {
+  const upload = db
+    .get('uploads')
+    .find({ id })
+    .value();
+  db.get('uploads')
+    .remove(upload)
+    .write()
 };
 
 module.exports = {
   db,
-  processUpload
+  deleteFile,
+  saveFile
 };
