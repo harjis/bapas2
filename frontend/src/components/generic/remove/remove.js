d9f95f379e0f6b64d57c131ee6b335ea82ee4204
Joonas Harjukallio <harjukallio@gmail.com>
2018-06-20 21:54:03 +0300

    Move uploads under one container. TODO use Mutation component for delete upload

--- frontend/src/components/generic/remove/remove.js
+++ frontend/src/components/generic/remove/remove.js
@@ -3,6 +3,4 @@ import * as React from 'react';
 import styles from './remove.module.css';
 import remove from 'src/images/remove.svg';
 
-export default () => (
-  <img className={styles.icon} src={remove} />
-)
+export default props => <img className={styles.icon} src={remove} onClick={props.onClick} />;
