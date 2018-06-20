b8c4dd6ca88dfb7f6baa985c7ee35500f7e842f0
Joonas Harjukallio <harjukallio@gmail.com>
2018-06-20 20:23:22 +0300

    Add process and remove buttons

--- frontend/src/components/upload/uploads.js
+++ frontend/src/components/upload/uploads.js
@@ -4,16 +4,20 @@ import PropTypes from 'prop-types';
 import { compose } from 'recompose';
 import { graphql } from 'react-apollo/index';
 
+import Button from '../generic/button/button';
 import RemoveIcon from '../generic/remove/remove';
 import Loading from '../generic/loading/loading';
 
 import styles from './uploads.module.css';
 
-const TableRow = upload => (
+const TableRow = ({ upload, onRemove }) => (
   <tr key={upload.filename}>
     <td>{upload.filename}</td>
-    <td className={styles.removeIcon}>
-      <RemoveIcon />
+    <td className={styles.controlButtons}>
+      <Button onClick={() => {}}>Process</Button>
+    </td>
+    <td className={styles.controlButtons}>
+      <RemoveIcon onClick={onRemove.bind(upload.id)} />
     </td>
   </tr>
 );
@@ -25,28 +29,38 @@ const Uploads = props => {
       <div className={styles.titleContainer}>Uploads</div>
       <table>
         <thead>
-          <th>Filename</th>
-          <th>Remove</th>
+          <tr>
+            <th>Filename</th>
+            <th>Process</th>
+            <th>Remove</th>
+          </tr>
         </thead>
-        <tbody>{props.data.uploads.map(TableRow)}</tbody>
+        <tbody>
+          {props.data.uploads.map(upload => (
+            <TableRow key={upload.id} upload={upload} onRemove={props.onRemove} />
+          ))}
+        </tbody>
       </table>
     </div>
   );
 };
 
 Uploads.propTypes = {
-  data: PropTypes.object.isRequired
+  data: PropTypes.object.isRequired,
+  onRemove: PropTypes.func.isRequired
 };
 
 class UploadsContainer extends React.Component {
+  handleRemove = () => {};
   render() {
-    return <Uploads data={this.props.data} />;
+    return <Uploads data={this.props.data} onRemove={this.handleRemove} />;
   }
 }
 
 const UPLOAD = gql`
   query {
     uploads {
+      id
       filename
     }
   }
