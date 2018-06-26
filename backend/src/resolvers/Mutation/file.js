dd4b0fdb85b8352ea7a13fefc5120ae3455b06fc
Joonas Harjukallio <harjukallio@gmail.com>
2018-06-26 15:48:47 +0300

    Rename singleUpload to addUpload. Update cache on addUpdate

--- backend/src/resolvers/Mutation/file.js
+++ backend/src/resolvers/Mutation/file.js
@@ -1,6 +1,6 @@
 const { deleteFile, saveFile } = require('../../file_upload');
 const file = {
-  singleUpload: (obj, { file }) => saveFile(file),
+  addUpload: (obj, { file }) => saveFile(file),
   multipleUpload: (obj, { files }) => Promise.all(files.map(saveFile)),
   deleteUpload: (obj, { id }) => deleteFile(id),
 };
