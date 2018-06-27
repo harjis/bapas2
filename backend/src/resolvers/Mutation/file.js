1df94e0dd77006ecaa227e5907e01415cb85d1ec
Joonas Harjukallio <harjukallio@gmail.com>
2018-06-27 20:21:12 +0300

    Require logging in

--- backend/src/resolvers/Mutation/file.js
+++ backend/src/resolvers/Mutation/file.js
@@ -1,35 +1,28 @@
 const saveFile = require('../../file_upload/save_file');
 const deleteFile = require('../../file_upload/delete_file');
-const processFile = require('../../file_upload/process_file');
+const PaymentBatchParser = require('../../parsers/payment_batch_parser');
+
 const { getUserId } = require('../../utils');
 
-const createOrUseExistingAccount = async (ctx, line, userId) => {
-  const iban = line.split("\t")[1];
-  const account = await ctx.db.query.account({ where: { iban } })
-  if (account) return account;
-  return await ctx.db.mutation.createAccount(
-    {
-      data: {
-        iban,
-        user: {
-          connect: { id: userId }
-        }
-      }
-    }
-  );
-};
-
 const file = {
-  addUpload: (obj, { file }) => saveFile(file),
-  multipleUpload: (obj, { files }) => Promise.all(files.map(saveFile)),
-  deleteUpload: (obj, { id }) => deleteFile(id),
-  async processUpload(parent, { id }, ctx, info) {
-    const userId = getUserId(ctx);
-    let account;
-    processFile(id, (line, lineNumber) => {
-      if (lineNumber === 1) account = createOrUseExistingAccount(ctx, line, userId);
-    });
+    addUpload: (parent, { file }, ctx) => {
+      const userId = getUserId(ctx);
+      return saveFile(file)
+    },
+    multipleUpload: (parent, { files }, ctx) => {
+      const userId = getUserId(ctx);
+      return Promise.all(files.map(saveFile))
+    },
+    deleteUpload: (parent, { id }, ctx) => {
+      const userId = getUserId(ctx);
+      return deleteFile(id)
+    },
+    processUpload: (parent, { id }, ctx, info) => {
+      const userId = getUserId(ctx);
+      const parser = new PaymentBatchParser(ctx, id, userId);
+      parser.parse();
+    }
   }
-};
+;
 
 module.exports = file;
