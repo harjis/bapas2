798e57a807b1261ec7ec619722fbdc6c6d00de0a
Joonas Harjukallio <harjukallio@gmail.com>
2018-06-29 16:02:02 +0300

    First version from file upload

--- backend/src/file_upload/process_file.js
+++ backend/src/file_upload/process_file.js
@@ -1,13 +1,14 @@
-const readline = require('readline');
-const fs = require('fs');
-const { getFilePath } = require('./init');
+const fs = require("fs");
+const { getFilePath } = require("./init");
 
-const processFile = (id, callback) => {
-  const rl = readline.createInterface({
-    input: fs.createReadStream(getFilePath(id))
-  });
-  let lineNumber = 1;
-  rl.on('line', (line) => callback(line, lineNumber++));
+const processFile = id => {
+  return fs
+    .readFileSync(getFilePath(id), "utf-8")
+    .trim()
+    .split("\n")
+    .filter(line => {
+      return line !== "\r";
+    });
 };
 
 module.exports = processFile;
