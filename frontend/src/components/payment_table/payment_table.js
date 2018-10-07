542ab8ba785bde0314a79b6ada5d5d61b469f093
Joonas Harjukallio <harjukallio@gmail.com>
2018-10-07 19:10:40 +0300

    Add sorting to PaymentTable

--- frontend/src/components/payment_table/payment_table.js
+++ frontend/src/components/payment_table/payment_table.js
@@ -4,18 +4,24 @@ import PropTypes from 'prop-types';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
-import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
+
+import SortableTableHead from './sortable_table_head';
+
+const headers = [
+  { id: 'name', numeric: false, label: 'Name' },
+  { id: 'amount', numeric: true, label: 'Amount' }
+];
 
 const PaymentTable = props => (
   <Paper>
     <Table>
-      <TableHead>
-        <TableRow>
-          <TableCell>Name</TableCell>
-          <TableCell numeric>Amount</TableCell>
-        </TableRow>
-      </TableHead>
+      <SortableTableHead
+        headers={headers}
+        orderBy={props.orderBy}
+        order={props.order}
+        onSort={props.onSort}
+      />
       <TableBody>
         {props.payments.map(row => (
           <TableRow key={row.id}>
@@ -31,6 +37,9 @@ const PaymentTable = props => (
 );
 
 PaymentTable.propTypes = {
+  onSort: PropTypes.func.isRequired,
+  order: PropTypes.string.isRequired,
+  orderBy: PropTypes.string.isRequired,
   payments: PropTypes.array.isRequired
 };
 
--- frontend/src/components/payment_table/payment_table_container.js
+++ frontend/src/components/payment_table/payment_table_container.js
@@ -1,17 +1,47 @@
 import * as React from 'react';
-import { compose } from 'recompose';
-import { graphql } from 'react-apollo/index';
+import { Query } from 'react-apollo/index';
 
 import Loading from 'src/components/generic/loading/loading';
 import PaymentTable from './payment_table';
 import { GET_PAYMENTS } from 'src/queries/payment_queries';
-import { withErrorLogging } from 'src/components/generic/errors/errors';
+import Errors from 'src/components/generic/errors/errors';
 
 class PaymentTableContainer extends React.Component {
+  state = {
+    order: 'asc',
+    orderBy: 'amount',
+    orderByGQL: 'amount_ASC'
+  };
+
+  handleRequestSort = (event, property) => {
+    const orderBy = property;
+    let order = 'desc';
+
+    if (this.state.orderBy === property && this.state.order === 'desc') {
+      order = 'asc';
+    }
+
+    this.setState({ order, orderBy, orderByGQL: `${orderBy}_${order.toUpperCase()}` });
+  };
+
   render() {
-    if (this.props.data.loading) return <Loading />;
-    return <PaymentTable payments={this.props.data.payments} />;
+    return (
+      <Query variables={{ orderBy: this.state.orderByGQL }} query={GET_PAYMENTS}>
+        {({ loading, error, data }) => {
+          if (loading) return <Loading />;
+          if (error) return <Errors errors={error} />;
+          return (
+            <PaymentTable
+              order={this.state.order}
+              orderBy={this.state.orderBy}
+              onSort={this.handleRequestSort}
+              payments={data.payments}
+            />
+          );
+        }}
+      </Query>
+    );
   }
 }
 
-export default compose(graphql(GET_PAYMENTS), withErrorLogging)(PaymentTableContainer);
+export default PaymentTableContainer;
