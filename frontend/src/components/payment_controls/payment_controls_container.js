2cd5cbac3c4e8e5603ccc29d6434922c490d981b
Joonas Harjukallio <harjukallio@gmail.com>
2018-10-08 21:29:57 +0300

    Get available month and year from backend

--- frontend/src/components/payment_controls/payment_controls_container.js
+++ frontend/src/components/payment_controls/payment_controls_container.js
@@ -1,17 +1,47 @@
 import * as React from 'react';
+import { Query } from 'react-apollo/index';
 
+import Errors from 'src/components/generic/errors/errors';
+import Loading from 'src/components/generic/loading/loading';
 import PaymentControls from './payment_controls';
+import { GET_PAYMENT_DATES } from '../../queries/payment_queries';
+
 export default class PaymentControlsContainer extends React.Component {
   render() {
     return (
-      <PaymentControls
-        availableMonths={['1', '2']}
-        availableYears={['2009', '2010']}
-        month={'1'}
-        onSelectMonth={() => {}}
-        onSelectYear={() => {}}
-        year={'2009'}
-      />
+      <Query query={GET_PAYMENT_DATES}>
+        {({ loading, error, data }) => {
+          if (loading) return <Loading />;
+          if (error) return <Errors errors={error} />;
+          const monthsByYear = getMonthsByYear(data.payments);
+          const year = Object.keys(monthsByYear)[0];
+          return (
+            <PaymentControls
+              month={monthsByYear[year][0]}
+              monthsByYear={monthsByYear}
+              onSelectMonth={() => {}}
+              onSelectYear={() => {}}
+              year={year}
+            />
+          );
+        }}
+      </Query>
     );
   }
 }
+
+// TODO Seriously. There does not seem to be way to get distinct values
+const getMonthsByYear = dates =>
+  dates.reduce((acc, cur) => {
+    const date = new Date(cur.paymentDate);
+    if (!acc[date.getFullYear()]) {
+      return {
+        ...acc,
+        [date.getFullYear()]: [date.getMonth()]
+      };
+    }
+    return {
+      ...acc,
+      [date.getFullYear()]: [...new Set(acc[date.getFullYear()].concat(date.getMonth()))]
+    };
+  }, {});
