16eee85c3d5b0277382bc5540d33cb3bab1cc6cc
Joonas Harjukallio <harjukallio@gmail.com>
2018-10-17 21:33:32 +0300

    Maybee this is the simplest solution

--- frontend/src/utils/auth.js
+++ frontend/src/utils/auth.js
@@ -1,19 +1,25 @@
-import { USER_ID, USER_TOKEN } from 'src/constants/user_constants';
+import { USER_ID, USER_TOKEN, USER_NAME } from 'src/constants/user_constants';
 
 export const isLoggedIn = () => {
   return localStorage.getItem(USER_ID) && localStorage.getItem(USER_TOKEN);
+};
+
+export const getUserName = () => {
+  return localStorage.getItem(USER_NAME);
 };
 
 export const getToken = () => {
   return localStorage.getItem(USER_TOKEN);
 };
 
-export const login = (id, token) => {
+export const login = (id, name, token) => {
   localStorage.setItem(USER_ID, id);
   localStorage.setItem(USER_TOKEN, token);
+  localStorage.setItem(USER_NAME, name);
 };
 
 export const logout = () => {
   localStorage.removeItem(USER_ID);
   localStorage.removeItem(USER_TOKEN);
+  localStorage.removeItem(USER_NAME);
 };
