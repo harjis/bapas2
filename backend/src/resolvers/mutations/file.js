b1cb134d05bfaf99b4dcb9455c6d5bac38aa2b7d
Joonas Harjukallio <harjukallio@gmail.com>
2019-01-25 16:34:58 +0200

    Make processFile to mark file processed

--- backend/src/resolvers/mutations/file.js
+++ backend/src/resolvers/mutations/file.js
@@ -5,24 +5,29 @@ const PaymentBatchParser = require('../../parsers/payment_batch_parser');
 const { getUserId } = require('../../utils');
 
 const file = {
-    addUpload: (parent, { file }, ctx, info) => {
-      const userId = getUserId(ctx);
-      return saveFile(ctx, info, file)
-    },
-    multipleUpload: (parent, { files }, ctx) => {
-      const userId = getUserId(ctx);
-      return Promise.all(files.map(saveFile))
-    },
-    deleteUpload: (parent, { id }, ctx) => {
-      const userId = getUserId(ctx);
-      return deleteFile(id, ctx)
-    },
-    processUpload: (parent, { id }, ctx, info) => {
-      const userId = getUserId(ctx);
-      const parser = new PaymentBatchParser(ctx, id, userId);
-      parser.parse();
+  addUpload: (parent, { file }, ctx, info) => {
+    const userId = getUserId(ctx);
+    return saveFile(ctx, info, file)
+  },
+  multipleUpload: (parent, { files }, ctx) => {
+    const userId = getUserId(ctx);
+    return Promise.all(files.map(saveFile))
+  },
+  deleteUpload: (parent, { id }, ctx) => {
+    const userId = getUserId(ctx);
+    return deleteFile(id, ctx)
+  },
+  async processUpload(parent, { id }, ctx, info) {
+    const userId = getUserId(ctx);
+    const parser = new PaymentBatchParser(ctx, id, userId);
+    try {
+      await parser.parse();
+    } catch (e) {
+      throw new Error('Parsing failed');
     }
+
+    return ctx.db.mutation.updateFile({ where: { id }, data: { hasBeenProcessed: true } }, info);
   }
-;
+};
 
 module.exports = file;
