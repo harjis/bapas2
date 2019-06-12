23946a8c62c8299661e85c33a482536d1c466b28
Joonas Harjukallio <harjukallio@gmail.com>
2019-06-12 21:40:41 +0300

    Add User entity

--- backend2/src/server.ts
+++ backend2/src/server.ts
@@ -4,6 +4,10 @@ import depthLimit from 'graphql-depth-limit';
 import { createServer } from 'http';
 import compression from 'compression';
 import cors from 'cors';
+import "reflect-metadata";
+import {createConnection} from "typeorm";
+import {User} from "./entity/User";
+
 import schema from './schema';
 
 const app = express();
@@ -18,3 +22,23 @@ const httpServer = createServer(app);
 httpServer.listen(
   { port: 3000 },
   (): void => console.log(`\n🚀      GraphQL is now running on http://localhost:3000/graphql`));
+
+
+
+createConnection().then(async connection => {
+
+  console.log("Inserting a new user into the database...");
+  const user = new User();
+  user.firstName = "Timber";
+  user.lastName = "Saw";
+  user.age = 25;
+  await connection.manager.save(user);
+  console.log("Saved a new user with id: " + user.id);
+
+  console.log("Loading users from the database...");
+  const users = await connection.manager.find(User);
+  console.log("Loaded users: ", users);
+
+  console.log("Here you can setup and run express/koa/any other framework.");
+
+}).catch(error => console.log(error));
