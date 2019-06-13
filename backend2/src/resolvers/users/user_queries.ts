fb49eb246f03bdde4be942a2d629ce9f8f62749b
Joonas Harjukallio <harjukallio@gmail.com>
2019-06-13 20:36:25 +0300

    Add currentUser

--- backend2/src/resolvers/users/user_queries.ts
+++ backend2/src/resolvers/users/user_queries.ts
@@ -2,7 +2,11 @@ import { getManager } from "typeorm";
 
 import { User } from "../../entities/User";
 
+type Args = { id: number };
 const userQueries = {
+  currentUser(_: void, args: Args): Promise<User> {
+    return getManager().findOneOrFail(User, args.id);
+  },
   users(_: void, args: void): Promise<User[]> {
     return getManager().find(User);
 
