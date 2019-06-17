cae3055b6b48cb30d8c81d751af66a651b19ff57
Joonas Harjukallio <harjukallio@gmail.com>
2019-06-17 16:09:26 +0300

    Does something

--- backend2/src/resolvers/users/user_queries.ts
+++ backend2/src/resolvers/users/user_queries.ts
@@ -7,7 +7,7 @@ const userQueries = {
   currentUser(_: void, args: Args): Promise<User> {
     return getManager().findOneOrFail(User, args.id);
   },
-  users(_: void, args: void): Promise<User[]> {
+  async users(_: void, args: void): Promise<User[]> {
     return createQueryBuilder(User)
       .leftJoinAndSelect("User.accounts", "account")
       .getMany();
--- backend2/src/server.ts
+++ backend2/src/server.ts
@@ -1,24 +1,33 @@
 import dotenv from 'dotenv';
-import express from 'express';
-import { ApolloServer } from 'apollo-server-express';
-import depthLimit from 'graphql-depth-limit';
-import { createServer } from 'http';
-import compression from 'compression';
-import cors from 'cors';
-import "reflect-metadata";
-
-import schema from './schema';
-import { createConnection } from "typeorm";
 
 dotenv.config({ path: '.env' });
+
+import "reflect-metadata";
+import compression from 'compression';
+import cors from 'cors';
+import depthLimit from 'graphql-depth-limit';
+import express from 'express';
+import { ApolloServer } from 'apollo-server-express';
+import { createConnection } from "typeorm";
+import { createServer } from 'http';
+
+import schema from './schema';
+import { AuthError, initialize, authorize } from "./authorization";
 
 createConnection().then(async connection => {
   const app = express();
   const server = new ApolloServer({
     schema,
     validationRules: [ depthLimit(7) ],
+    context: context => {
+      if (!context.req.headers.authorization){
+        throw new AuthError('No Authorization token provided');
+      }
+      return authorize(context.req.headers.authorization);
+    }
   });
   app.use('*', cors());
+  initialize(app);
   app.use(compression());
   server.applyMiddleware({ app, path: '/graphql' });
   const httpServer = createServer(app);
